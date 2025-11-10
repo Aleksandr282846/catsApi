87771626cbf7dd2312d1be06deeb58fe1a1a2b9f
Aleksandr <aleksandr282846@gmail.com>
2025-11-10 19:01:23 +0200

    Second commit

--- src/screens/Breeds.tsx
+++ src/screens/Breeds.tsx
@@ -1,29 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+type Breed = {
+  id: string;
+  name: string;
+  description: string;
+}
 
 export const LoadBreeds = () => {
-  const [breeds, setBreeds] = useState();
+  const [breeds, setBreeds] = useState<Breed[]>();
 
   const url = 'https://api.thecatapi.com/v1/breeds';
 
-  axios
-    .get(url)
-    .then((response) => {
-      if (response?.data) {
-        setBreeds(response?.data)
-      }
-    })
-    .catch((error) => console.log(error));
-
   useEffect(() => {
-    LoadBreeds();
+    axios
+      .get(url)
+      .then((response) => {
+        if (response?.data) {
+          setBreeds(response?.data)
+        }
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      {breeds?.map(breed => (
+        <Text>{breed.name}</Text>
+      ))}
       <Image style={styles.image} source={{ uri: ''}}/>
-    </View>
+    </ScrollView>
   );
 }
 
